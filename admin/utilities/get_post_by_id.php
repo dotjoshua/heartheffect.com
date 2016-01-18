0386b64e415a86f7a1023e08e2042d2a8fda57a9
Joshua Diaddigo <joshuad95@hotmail.com>
2016-01-18 15:20:17 -0500

    Acclimating for live environment

--- admin/utilities/get_post_by_id.php
+++ admin/utilities/get_post_by_id.php
@@ -6,4 +6,4 @@ require_once "auth.php";
 
 $id = preg_replace("/[^0-9]+/", "", $_GET["post_id"]);
 
-echo query("SELECT * FROM posts WHERE id = ".$id, $DB_PASSWD, false);
+echo query("SELECT * FROM Posts WHERE id = ".$id, $DB_PASSWD, false);
